83aa7b4e46854daecf12872ace42e0da90567dd5
Alejandro Ruete <alejandro@greensway.se>
2021-02-24 14:17:59 +0100

    no travis, yes github actions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # shinyBIRDS
+
+[![R-CMD-check](https://github.com/Greensway/shinyBIRDS/workflows/R-CMD-check/badge.svg)](https://github.com/Greensway/shinyBIRDS/actions)
+
+
 An interactive interface for BIRDS (Biodiversity Information Review and Decision Support)
--- inst/shinyBIRDS/ui.R
+++ inst/shinyBIRDS/ui.R
@@ -15,8 +15,8 @@ navbarPage(title=div(img(src="./img/BirdsLogo.png", alt="", style="vertical-alig
                        href="https://use.fontawesome.com/releases/v5.1.0/css/all.css", 
                        integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt", 
                        crossorigin="anonymous"),
-             useShinyjs(),
              useShinydashboard(),
+             useShinyjs(),
              useShinyalert() ## deprecated
            ),
            
